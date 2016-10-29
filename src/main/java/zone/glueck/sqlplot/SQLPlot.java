9d1da7f0216d76544d06bfc1d901b0df749ae70c
Zach Glueckert <zachrg@gmail.com>
2016-10-29 06:18:20 -0400

    Wrap clipboard parsing and database load to background thread.

--- src/main/java/zone/glueck/sqlplot/SQLPlot.java
+++ src/main/java/zone/glueck/sqlplot/SQLPlot.java
@@ -9,6 +9,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.*;
+import java.util.List;
 
 /**
  * Created by zach on 10/23/16.
@@ -33,10 +35,55 @@ public class SQLPlot {
         captureClipboard.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                ClipboardParser clipboardParser = ClipboardParser.parse();
-                if (clipboardParser != null) {
-                    SQLController.getController().addData(clipboardParser, "clipboard");
+
+                String tableName = JOptionPane.showInputDialog("Table Name", "clipboard");
+                if (tableName == null || tableName.isEmpty()) {
+                    tableName = "clipboard";
                 }
+                final String workerTableName = tableName;
+
+                JDialog statusDialog = new JDialog(frame, "Clipboard Import", false);
+                statusDialog.setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+                statusDialog.setLayout(new GridBagLayout());
+                final Insets insets = new Insets(5,5,5,5);
+                GridBagConstraints c = new GridBagConstraints();
+                c.insets = insets;
+                c.weightx = 0.5;
+                c.gridx = 0;
+                c.gridy = 0;
+                c.fill = GridBagConstraints.HORIZONTAL;
+                c.anchor = GridBagConstraints.CENTER;
+                JProgressBar progressBar = new JProgressBar(0, 99);
+                progressBar.setString("Parsing clipboard data...");
+                statusDialog.add(progressBar, c);
+                statusDialog.setVisible(true);
+
+                SwingWorker<Void, Void> parseOperation = new SwingWorker<Void, Void>() {
+                    @Override
+                    protected Void doInBackground() throws Exception {
+
+                        ClipboardParser clipboardParser = ClipboardParser.parse();
+                        this.publish((Void) null);
+                        if (clipboardParser != null) {
+                            SQLController.getController().addData(clipboardParser, workerTableName);
+                        }
+
+                        return null;
+                    }
+
+                    @Override
+                    protected void process(List<Void> chunks) {
+                        progressBar.setString("Loading into database...");
+                    }
+
+                    @Override
+                    protected void done() {
+                        statusDialog.dispose();
+                    }
+                };
+
+                parseOperation.execute();
+
             }
         });
         fileMenu.add(captureClipboard);
